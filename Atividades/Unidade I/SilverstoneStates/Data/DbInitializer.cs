b6183d8a2e2dcffaf56cf4c3a89e7b614ead10ee
411730 <411730@VDA-LCOII-19168>
2025-08-29 22:12:29 -0300

    Student CRUD implemented

--- Atividades/Unidade I/SilverstoneStates/Data/DbInitializer.cs
+++ Atividades/Unidade I/SilverstoneStates/Data/DbInitializer.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using SilverstoneStates.Models;
+﻿using SilverstoneStates.Models;
 
 namespace SilverstoneStates.Data
 {
@@ -14,7 +13,7 @@ namespace SilverstoneStates.Data
 
             var realties = new Realty[]
             {
-                new() { Id = 1, Name = "Casa de Campo", Description = "Uma bela casa com 3 quartos e vista para as montanhas.", NumberOfRooms = 3, Market_Value = 75000 },
+                new() { Id = 1, Name = "Casa de Campo", Description = "Uma bela casa com 3 quartos e vista para as montanhas.", NumberOfRooms = 3, Market_Value = 75000.00m },
                 new() { Id = 2, Name = "Apartamento no Centro", Description = "Apartamento moderno de 2 quartos no coração da cidade.", NumberOfRooms = 2, Market_Value = 480000.50m },
                 new() { Id = 3, Name = "Loft Moderno", Description = "Loft com design industrial, espaço aberto e pé-direito alto.", NumberOfRooms = 1, Market_Value = 920000.00m },
                 new() { Id = 4, Name = "Sobrado Geminado", Description = "Sobrado com 4 quartos, ideal para famílias grandes.", NumberOfRooms = 4, Market_Value = 650000.00m },
@@ -22,9 +21,9 @@ namespace SilverstoneStates.Data
                 new() { Id = 6, Name = "Cobertura Duplex", Description = "Cobertura com piscina privativa e área gourmet.", NumberOfRooms = 3, Market_Value = 1800000.00m }
             };
 
-            foreach (Realty s in realties)
+            foreach (Realty r in realties)
             {
-                context.Realties.Add(s);
+                context.Realties.Add(r);
             }
             context.SaveChanges();
         }
